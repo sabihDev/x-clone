60be2e53e1b8fe4c4e31a8760ff64134cf148829
sabihDev <8203sabi.rahman@gmail.com>
2024-08-25 11:59:57 +0500

    followOrUnfollow controller created

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -14,3 +14,33 @@ export const getUserProfile = async (req, res) => {
     }
 }
 
+export const followOrUnfollow = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+
+        const user = await User.findById(id);
+        const me = await User.findById(userId);
+        if (!user && !me) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if(id === userId){
+            return res.status(400).json({ message: "You can't follow yourself" });
+        }
+
+        // check if already followed
+        const isFollowing = me.following.includes(id);
+        if (isFollowing) {
+            await user.updateOne({ $pull: { followers: userId } });
+            await me.updateOne({ $pull: { following: id } });
+        } else {
+            await user.updateOne({ $push: { followers: userId } });
+            await me.updateOne({ $push: { following: id } });
+        }
+        return res.status(200).json({ message: "Followed or Unfollowed" });
+    } catch (error) {
+        console.log("Error in followOrUnfollow controller", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
