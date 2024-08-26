1b1d9a01fc2c70f9655096f0b69c5d5d94d1dfd6
sabihDev <8203sabi.rahman@gmail.com>
2024-08-26 12:15:16 +0500

    post model created

---
+++ backend/models/post.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text:{
+        type: String,
+    },
+    image:{
+        type: String,
+    },
+    likes:[{type: mongoose.Schema.Types.ObjectId, ref:"User",default:[]}],
+    comments:[{
+        text:{
+            type: String,
+        },
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+    }],
+},{timestamps: true});  
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
