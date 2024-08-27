51e654fcb6bc77a2287514cbb9e338411804b9db
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 14:02:49 +0500

    get notifications controller created

--- backend/controllers/notification.controllers.js
+++ backend/controllers/notification.controllers.js
@@ -1,10 +1,20 @@
+import Notification from "../models/notification.model.js";
+
 export const getNotifications = async (req, res) => {
     try {
         const userId = req.user._id;
 
-        const notifications = 
+        const notifications = await Notification.find({ to: userId }).populate({
+            path: "from",
+            select: "username profileImage",
+        });
+
+        await Notification.updateMany({ to: userId }, { read: true });
+
+        res.status(200).json({ notifications });
     } catch (error) {
-        
+        console.log("Error in getNotifications controller", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });        
     }
 };
 
