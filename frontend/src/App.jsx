d77de91865a4bcdd79153ac94e21d77cf8ad306e
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 16:24:54 +0500

    Home page design started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,13 @@ import HomePage from './pages/home/HomePage'
 import SignupPage from './pages/auth/signup/SignupPage'
 import LoginPage from './pages/auth/login/LoginPage'
 
+import Sidebar from './components/common/Sidebar'
+
 function App() {
 
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
       <Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignupPage />} />
