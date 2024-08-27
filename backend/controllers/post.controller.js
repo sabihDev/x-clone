539b4ec713f12d90d3eeec3f4e2dbb19e211b869
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 10:19:21 +0500

    delete post controller created

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
 import {v2 as cloudinary} from "cloudinary";
 
 export const createPost = async (req, res) => {
@@ -32,5 +33,30 @@ export const createPost = async (req, res) => {
     } catch (error) {
         console.log("Error in createPost controller", error.message);
         return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        if(post.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        if(post.img){
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully" });
+    } catch (error) {
+        console.log("Error in deletePost controller", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });        
     }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -6,7 +6,7 @@ import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
-router.delete("/delete/:id", protectRoute, deletePost);
+router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likepost);
 router.post("/comment/:id", protectRoute, commentPost);
 
