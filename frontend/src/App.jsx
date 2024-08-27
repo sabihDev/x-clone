85a6a3f34c9115a0e2c11279b0a7aa99835e6663
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 17:17:46 +0500

    profile page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import NotificationPage from './pages/notification/NotificationPage'
 
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import ProfilePage from './pages/profile/ProfilePage'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
 				<Route path='/signup' element={<SignupPage />} />
 				<Route path='/login' element={<LoginPage />} />
         <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
 			</Routes>
       <RightPanel/>
     </div>
