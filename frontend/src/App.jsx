3d35054f313c630b5e84678c9f8a6ed003dfe004
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 16:09:25 +0500

    react-router-dom setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+
+import HomePage from './pages/home/HomePage'
+import SignupPage from './pages/auth/signup/SignupPage'
+import LoginPage from './pages/auth/login/LoginPage'
+
 function App() {
 
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+      <Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignupPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+    </div>
   )
 }
 
