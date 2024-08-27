22d9b86813d77dcf86fd75f975abb3e9773ae9db
sabihDev <8203sabi.rahman@gmail.com>
2024-08-27 14:05:38 +0500

    delete notification controller created

--- backend/controllers/notification.controllers.js
+++ backend/controllers/notification.controllers.js
@@ -18,4 +18,15 @@ export const getNotifications = async (req, res) => {
     }
 };
 
-export const deleteNotifications = async (req, res) => {};
+export const deleteNotifications = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        await Notification.deleteMany({ to: userId });
+
+        res.status(200).json({ message: "Notifications deleted successfully" });
+    } catch (error) {
+        console.log("Error in deleteNotifications controller", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });        
+    }
+};
